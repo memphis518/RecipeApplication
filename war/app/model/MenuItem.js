d6ef683d826ad2509a59dda4647c64ecdf58d566
Stephen Watters <stephen.watters@gmail.com>
2011-12-16 08:35:32 -0600

    Need to commit this to pull it up on another machine

---
+++ war/app/model/MenuItem.js
@@ -0,0 +1,7 @@
+Ext.define('RA.model.MenuItem',{
+        extend: 'Ext.data.Model',
+        fields : [
+            {name : 'name', type : 'string'},
+            {name : 'detail', type: 'string'}
+        ]
+    });
